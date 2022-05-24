8ad3c456934bd57dfe58a3faa963c650c7236a2b
TudbuT <tudbut@tudbut.de>
2022-05-24 18:58:35 +0200

    make command line app, improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
-cp /bin/isbpl ISBPL.jar
-javac -cp ISBPL.jar Wrapper.java 
+IFS=: read -r BIN OTHERBIN <<< "$PATH"
+mkdir /usr/lib/isbpl >& /dev/null
+touch /usr/lib/isbpl/test && export BIN=/usr/bin && rm /usr/lib/isbpl/test
+echo "Installing to $BIN."
+
+cp "$BIN"/isbpl ISBPL.jar
 mkdir tmp ; cd tmp
-    unzip ../ISBPL.jar 
-    rm ../ISBPL.jar
-    cp ../*.class .
+    cp ../Wrapper.java ../ISBPL.jar .
+    javac -cp ISBPL.jar Wrapper.java 
+    unzip ISBPL.jar 
+    rm ISBPL.jar ../ISBPL.jar
     zip -r ../ISBPL.jar *
 cd ..
 rm -rf tmp
+cat > "$BIN"/imagelayer << "EOF"
+cd $(pwd)
+java -jar ISBPL.jar main.isbpl $@
+EOF
+chmod a+rx "$BIN"/imagelayer
